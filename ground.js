fda21af7a190e790c81f070b406c3f6cdb62d137
SP2007 <74725161+SP2007@users.noreply.github.com>
2021-01-27 14:05:53 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,19 @@
+class Ground{
+
+    constructor(){
+        var options={
+            isStatic : true
+          }
+        
+          this.body = Bodies.rectangle(450,390,900,20,options);
+          this.width = 900;
+          this.height = 20;
+          World.add(world,this.body);
+    }
+    display(){
+        noStroke();
+        fill(188,67,67);
+        rectMode(CENTER);
+        rect(this.ground.position.x,this.ground.position.y,900,20);
+    }
+}
